2a4e201f8fbbb1a3dd90453d4ef38a2a2ee385f5
Shanawaz Raza <115471673+shanawaz-official@users.noreply.github.com>
2022-10-19 18:52:16 +0530

    Add files via upload

---
+++ Android Clone/app.js
@@ -0,0 +1,49 @@
+let menuIcon=document.querySelector('.menu-box');
+
+let mainDiv=document.querySelector('#main');
+
+//menuIcon.addEventListener("click",()=>{
+  //console.log("hi")
+//});
+
+function run(){
+  prompt("run")
+  //mainDiv.setAttribute('class','main2');
+  //mainDiv.style.bottom == "-10px";
+  mainDiv.className="main2";
+};
+  
+  /*mainDiv.
+     if (mainDiv.style.bottom == "570px"){
+       mainDiv.style.bottom == "-10px"
+     }
+     else {
+       mainDiv.style.bottom == "570px"
+     }
+  
+};
+
+if (menuIcon) {
+  menuIcon.addEventListener('click', ()=> {
+    prompt("hu");
+    if (mainDiv.style.bottom=="570px") {
+      mainDiv.style.bottom=="-10px"
+    }
+    else{
+      mainDiv.style.bottom=="570px"
+    }
+  });
+}
+
+// ✅ Using optional chaining (?.)
+menuIcon?.addEventListener('click', ()=> {
+  prompt("hu");
+      if (mainDiv.style.bottom == "570px") {
+        mainDiv.style.bottom == "-10px"
+      }
+      else {
+        mainDiv.style.bottom == "570px"
+      }
+});*/
+
+
